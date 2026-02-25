d5d90333f434e21212c821d07c6c0715b17fd959
harish1234-aids <ragavharish@gmail.com>
2026-02-25 18:50:54 +0530

    g

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/game.html')
-def game():
-    return render_template('game.html')
+@app.route('/hunt')
+def hunt():
+    return render_template('game.html') # The mini-game code you shared
 
 if __name__ == '__main__':
     app.run(debug=True)
